793e07630da7964023fdd4a9de16533a06b1dae8
connectwithdeepak1 <connectwithdeepak1@gmail.com>
2025-05-01 14:40:43 +0000

    imported pyrogram filters in the __init__.py file

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,6 +1,7 @@
+from pyrogram import filters
 from pyrogram import Client as stark
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from main import LOGGER, prefixes, AUTH_USERS
+from main import LOGGER
 from config import Config
 import os
 import sys
