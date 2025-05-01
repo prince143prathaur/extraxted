1e96fa63485cf9749fb6a4fd2ec02750515f7b59
connectwithdeepak1 <connectwithdeepak1@gmail.com>
2025-05-01 09:40:02 +0000

    Changed the AUTH_USERS in config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
     API_ID = int(os.environ.get("API_ID", "7856100"))
     API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
-    AUTH_USERS = "1411895712"
+    AUTH_USERS = "6473502061"
 
