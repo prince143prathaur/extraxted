206768ced1e901c072ce9fd454962a51cc78a6a8
connectwithdeepak1 <connectwithdeepak1@gmail.com>
2025-05-01 15:50:14 +0000

    Resetted config.json file

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8018549150:AAFI8u3S6BGvVfMxDgvkbOIYYttO7zBr6dM")
-    API_ID = int(os.environ.get("API_ID", "21103196"))
-    API_HASH = os.environ.get("API_HASH", "d0f94f6d2c3644e3e4e90d4b8be424ac")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
+    API_ID = int(os.environ.get("API_ID", "7856100"))
+    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
     AUTH_USERS = "6473502061"
     LOG_CHAT_ID = int(os.environ.get("LOG_CHAT_ID", "6473502061"))
 
